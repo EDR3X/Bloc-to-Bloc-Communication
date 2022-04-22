495686e958bd82da66b25baa45d0039d7216303c
EDR3X <dhunganaannanta@gmail.com>
2022-04-22 18:56:17 +0545

    initial commit

---
+++ lib/cubits/counter/counter_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+part 'counter_state.dart';
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(CounterState.initial());
+}
