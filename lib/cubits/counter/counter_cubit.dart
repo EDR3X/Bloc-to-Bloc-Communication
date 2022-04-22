b3189ca2a02ea07c0c24df96fcfafdaa044925fb
EDR3X <dhunganaannanta@gmail.com>
2022-04-22 18:59:56 +0545

    refactoring cubits

--- lib/cubits/counter/counter_cubit.dart
+++ lib/cubits/counter/counter_cubit.dart
@@ -1,9 +1,41 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:bloc_to_bloc_communication/cubits/color/color_cubit.dart';
 import 'package:equatable/equatable.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/material.dart';
 
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
-  CounterCubit() : super(CounterState.initial());
+  int incrementSize = 1;
+  final ColorCubit colorCubit;
+  late final StreamSubscription colorSubscription;
+
+  CounterCubit({
+    required this.colorCubit,
+  }) : super(CounterState.initial()) {
+    colorSubscription = colorCubit.stream.listen((ColorState colorState) {
+      if (colorState.color == Colors.red) {
+        incrementSize = 1;
+      } else if (colorState.color == Colors.green) {
+        incrementSize = 10;
+      } else if (colorState.color == Colors.blue) {
+        incrementSize = 50;
+      } else if (colorState.color == Colors.black) {
+        emit(state.copyWith(counter: state.counter - 100));
+        incrementSize = -100;
+      }
+    });
+  }
+
+  void changeCounter() {
+    emit(state.copyWith(counter: state.counter + incrementSize));
+  }
+
+  @override
+  Future<void> close() {
+    colorSubscription.cancel();
+    return super.close();
+  }
 }
