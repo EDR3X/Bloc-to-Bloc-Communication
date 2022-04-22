a367550403dc525e2e813e8b03242919bf80ee75
EDR3X <dhunganaannanta@gmail.com>
2022-04-22 19:31:56 +0545

    bloc implemented in UI

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,7 @@
+import 'package:bloc_to_bloc_communication/blocs/color/color_bloc.dart';
+import 'package:bloc_to_bloc_communication/blocs/counter/counter_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -6,13 +9,15 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
+      backgroundColor: context.watch<ColorBloc>().state.color,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                context.read<ColorBloc>().add(ChangeColorEvent());
+              },
               child: const Text(
                 "Change Color",
                 style: TextStyle(fontSize: 24.0),
@@ -21,18 +26,21 @@ class HomePage extends StatelessWidget {
             const SizedBox(
               height: 20.0,
             ),
-            const Text(
-              "0",
-              style: TextStyle(
-                  fontSize: 52,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white),
+            Text(
+              "${context.watch<CounterBloc>().state.counter}",
+              style: const TextStyle(
+                fontSize: 52,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
             ),
             const SizedBox(
               height: 20,
             ),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                context.read<CounterBloc>().add(ChangeCounterEvent());
+              },
               child: const Text(
                 "Increment Counter",
                 style: TextStyle(fontSize: 24),
