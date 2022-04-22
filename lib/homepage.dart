32aa9a12076d92ba0ccf84aeb42a4450b80a8a68
EDR3X <dhunganaannanta@gmail.com>
2022-04-22 19:15:36 +0545

    UI completed

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -5,6 +5,42 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text(
+                "Change Color",
+                style: TextStyle(fontSize: 24.0),
+              ),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            const Text(
+              "0",
+              style: TextStyle(
+                  fontSize: 52,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text(
+                "Increment Counter",
+                style: TextStyle(fontSize: 24),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
